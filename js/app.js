574b7b145fe011c06107d48463f313dd242268b4
Mike Newell <mike@iwearshorts.com>
2012-05-24 19:54:57 -0700

    updated bookmark to include easeljs sparkles, found out this isn't the direction we want to go...too many sites block cross domain scripts

---
+++ js/app.js
@@ -0,0 +1,118 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var App = function() {
+    
+    var $;
+    
+    var marioHand = function() {
+        
+        var styles =    '<style type="text/css">';
+            styles +=       'root { display: block; }';
+            styles +=       '#coins-header { width: 100%; height: 200px; background-color: black; }';
+            styles +=       '#cursor { position: absolute; }';
+            styles +=       '#coins-canvas { position: absolute; top: 1px; left: 1px; }';
+            styles +=   '</style>';
+        
+        $('head').append(styles);
+        $('body').prepend('<div id="coins-header"></div>');
+        $('body').append('<canvas id="coins-canvas" width="'+$(document).width()+'" height="'+$(document).height()+'"></canvas>');
+        
+        var easel = document.createElement('script');
+        easel.onload = function() {
+            // load in coin library
+            var coins = document.createElement('script');
+            coins.onload = function() {
+                // do something
+                var c = new Coins();
+                c.init();
+            }
+            coins.src = '//localhost.com/GSP/clients/nintendo/coin-orgy/production/js/coins.js';
+            document.getElementsByTagName('body')[0].appendChild(coins);
+        }
+        easel.src = 'http://code.createjs.com/easeljs-0.4.2.min.js';
+        document.getElementsByTagName('body')[0].appendChild(easel);
+        
+            
+        
+        
+    }
+    
+    var cursorTrack = function() {
+        $('body').css({'cursor' : 'none'});
+        $('body').append('<img id="cursor" src="//localhost.com/GSP/clients/nintendo/coin-orgy/production/images/cursor.png" alt="cursor" />');
+        $('body').append('<link href="//localhost.com/GSP/clients/nintendo/coin-orgy/production/css/demoStyles.css" rel="stylesheet" type="text/css" />');
+        $('body').append('');
+        $('body').on('mousemove', function(evt) {
+            $('#cursor').css({
+                'position' : 'absolute',
+                'top' : evt.pageY + 1, 
+                'left' : evt.pageX + 1
+            });
+        });
+
+    }
+    
+    this.init = function() {
+        // do stuff
+        if(typeof jQuery == 'undefined') {
+            console.debug('jquery no loaded, something went wrong');
+        } else {
+            console.debug('able to init the app!');
+            $ = jQuery;
+            marioHand();
+            cursorTrack();
+        }
+    }
+}
+
+if (!document.getElementById('coins-style')) {
+    var objHead = document.getElementsByTagName('head');
+    if (objHead[0]) {
+        var objCSS = objHead[0].appendChild(document.createElement('link'));
+        objCSS.id = 'coins-style';
+        objCSS.rel = 'stylesheet';
+        objCSS.href = '//localhost.com/GSP/clients/nintendo/coin-orgy/production/css/coins.css';
+        objCSS.type = 'text/css';
+    }
+}
+// load jquery
+var script = document.createElement('script');
+script.onload = function() {
+    console.debug('jquery loaded just now');
+    var a = new App();
+    a.init();
+};
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js";
+document.getElementsByTagName('body')[0].appendChild(script);
+
+
+//if(typeof jQuery == 'undefined') {
+//    
+//    
+//    
+//    // load jquery
+//    var script = document.createElement('script');
+//    script.onload = function() {
+//        console.debug('jquery loaded just now');
+//        var a = new App();
+//        a.init();
+//    };
+//    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js";
+//    document.getElementsByTagName('head')[0].appendChild(script);
+//} else {
+//    // load jquery
+//    var script = document.createElement('script');
+//    script.onload = function() {
+//        console.debug('jquery loaded just now');
+//        var a = new App();
+//        a.init();
+//    };
+//    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js";
+//    document.getElementsByTagName('head')[0].appendChild(script);
+//    
+//    var a = new App();
+//    a.init();
+//    console.debug('jquery loaded already');
+//}
--- js/coins.js
+++ js/coins.js
@@ -1,92 +1,120 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-var App = function() {
-    
-    var $;
-    
-    var marioHand = function() {
-        
-        var styles =    '<style type="text/css">';
-            styles +=       'root { display: block; }';
-            styles +=       '#coins-header { width: 100%; height: 200px; background-color: black; }';
-            styles +=       '#cursor { position: absolute; }';
-            styles +=   '</style>';
-        
-        $('head').append(styles);
-        $('body').prepend('<div id="coins-header"></div>');
-    }
-    
-    var playCoins = function() {
-        
-    }
-    
-    var cursorTrack = function() {
-        $('body').css({'cursor' : 'none'});
-        $('body').append('<img id="cursor" src="http://localhost.com/GSP/clients/nintendo/coin-orgy/v1/images/cursor.png" alt="cursor" />');
-        $('body').on('mousemove', function(evt) {
-            $('#cursor').css({
-                'position' : 'absolute',
-                'top' : evt.pageY + 1, 
-                'left' : evt.pageX + 1
-            });
-        });
-        
-        $('body').on('click', function(evt) {
-            playCoins();
-        });
-    }
-    
+var Coins = function() {
+    var canvas;
+    var stage;
+
+    var imgSeq = new Image();		// The image for the sparkle animation
+    var bmpAnim;						// The animated sparkle template to clone
+    var fpsLabel;
+
     this.init = function() {
-        // do stuff
-        if(typeof jQuery == 'undefined') {
-            console.debug('jquery no loaded, something went wrong');
-        } else {
-            console.debug('able to init the app!');
-            $ = jQuery;
-            marioHand();
-            cursorTrack();
-        }
+            
+            // create a new stage and point it at our canvas:
+            canvas = document.getElementById("coins-canvas");
+            stage = new Stage(canvas);
+            
+            // attach mouse handlers directly to the source canvas.
+            // better than calling from canvas tag for cross browser compatibility:
+            canvas.onmousemove = moveCanvas;
+            canvas.onclick = clickCanvas;
+
+            // define simple sprite sheet data specifying the image(s) to use, the size of the frames,
+            // and the registration point of the frame
+            // it will auto-calculate the number of frames from the image dimensions and loop them
+            var data = {
+                    images: ["//localhost.com/GSP/clients/nintendo/coin-orgy/production/images/sparkle_21x23.png"],
+                    frames: {width:21,height:23,regX:10,regY:11}
+            }
+            
+
+            // set up an animation instance, which we will clone
+            bmpAnim = new BitmapAnimation(new SpriteSheet(data));
+            
+
+            // add a text object to output the current FPS:
+            fpsLabel = new Text("-- fps","bold 14px Arial","#FFF");
+            stage.addChild(fpsLabel);
+            fpsLabel.x = 10;
+            fpsLabel.y = 20;
+            
+            // start the tick and point it at the window so we can do some work before updating the stage:
+            Ticker.setFPS(30);
+            Ticker.addListener(window);
+            
+    }
+
+
+    var tick = function() {
+        
+            // loop through all of the active sparkles on stage:
+            var l = stage.getNumChildren();
+            for (var i=l-1; i>0; i--) {
+                    var sparkle = stage.getChildAt(i);
+
+                    // apply gravity and friction
+                    sparkle.vY += 2;
+                    sparkle.vX *= 0.98;
+
+                    // update position, scale, and alpha:
+                    sparkle.x += sparkle.vX;
+                    sparkle.y += sparkle.vY;
+                    sparkle.scaleX = sparkle.scaleY = sparkle.scaleX+sparkle.vS;
+                    sparkle.alpha += sparkle.vA;
+
+                    //remove sparkles that are off screen or not invisble
+                    if (sparkle.alpha <= 0 || sparkle.y > canvas.height) {
+                            stage.removeChildAt(i);
+                    }
+            }
+
+            fpsLabel.text = Math.round(Ticker.getMeasuredFPS())+" fps";
+
+            // draw the updates to stage
+            stage.update();
+    }
+    
+    window.tick = tick;
+
+    //sparkle explosion
+    var clickCanvas = function (e) {
+        
+            addSparkles(Math.random()*200+100|0, stage.mouseX, stage.mouseY,2);
+    }
+
+    //sparkle trail
+    var moveCanvas = function (e) {
+        
+            addSparkles(Math.random()*2+1|0, stage.mouseX, stage.mouseY,1);
+    }
+
+    var addSparkles = function (count, x, y, speed) {
+        
+            //create the specified number of sparkles
+            for (var i=0; i<count; i++) {
+                    // clone the original sparkle, so we don't need to set shared properties:
+                    var sparkle = bmpAnim.clone();
+                    
+                    // set display properties:
+                    sparkle.x = x;
+                    sparkle.y = y;
+
+                    //sparkle.rotation = Math.random()*360;
+                    sparkle.alpha = Math.random()*0.5+0.5;
+                    sparkle.scaleX = sparkle.scaleY = Math.random()+0.3;
+
+                    // set up velocities:
+                    var a = Math.PI*2*Math.random();
+                    var v = (Math.random()-0.5)*30*speed;
+                    sparkle.vX = Math.cos(a)*v;
+                    sparkle.vY = Math.sin(a)*v;
+                    sparkle.vS = (Math.random()-0.5)*0.2; // scale
+                    sparkle.vA = -Math.random()*0.05-0.01; // alpha
+
+                    // start the animation on a random frame:
+                    sparkle.gotoAndPlay(Math.random()*sparkle.spriteSheet.getNumFrames()|0);
+
+                    // add to the display list:
+                    stage.addChild(sparkle);
+            }
     }
 }
 
-
-
-if(typeof jQuery == 'undefined') {
-    
-    if (!document.getElementById('coins-style')) {
-        var objHead = document.getElementsByTagName('head');
-        if (objHead[0]) {
-            var objCSS = objHead[0].appendChild(document.createElement('link'));
-            objCSS.id = 'coins-style';
-            objCSS.rel = 'stylesheet';
-            objCSS.href = '//localhost.com/GSP/clients/nintendo/coin-orgy/v1/css/coins.css';
-            objCSS.type = 'text/css';
-        }
-    }
-    
-    // load jquery
-    var script = document.createElement('script');
-    script.onload = function() {
-        console.debug('jquery loaded just now');
-        var a = new App();
-        a.init();
-    };
-    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js";
-    document.getElementsByTagName('head')[0].appendChild(script);
-} else {
-    // load jquery
-    var script = document.createElement('script');
-    script.onload = function() {
-        console.debug('jquery loaded just now');
-        var a = new App();
-        a.init();
-    };
-    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js";
-    document.getElementsByTagName('head')[0].appendChild(script);
-    
-    var a = new App();
-    a.init();
-    console.debug('jquery loaded already');
-}
