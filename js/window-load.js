bab835dbfcbb63030772a8d3d6722c2a1c2ccc0c
Mike Newell <mike@iwearshorts.com>
2012-05-31 19:47:23 -0700

    experimented with onload events in bookmarklets not possible

---
+++ js/window-load.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function() {
+    console.debug('window loaded');
+    setTimeout(function() {
+        console.debug('window loaded');
+    }, 1000);
+})();
